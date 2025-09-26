3f4d3dd5fdf9640ca93e5d87c9ebaef06548d933
billyw03 <yohanesbillywicaksono_2327240155@mhs.mdp.ac.id>
2025-09-26 20:12:37 +0700

    Pertemuan5
    latihan

--- toko-online/routes/index.js
+++ toko-online/routes/index.js
@@ -4,7 +4,29 @@ var products = require("../data/products.json");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Toko Online Sederhana", products: products });
+  res.render("index", {
+    title: "Toko Online Sederhana",
+    products: products,
+    query: null, //biar bisa dipakai di view
+  });
+});
+
+/* GET search page. */
+router.get("/search", function (req, res, next) {
+  const q = req.query.q ? req.query.q.toLowerCase() : "";
+
+  let filteredProducts;
+  if (!q) {
+    filteredProducts = products; // jika query kosong tampilkan semua
+  } else {
+    filteredProducts = products.filter((p) => p.name.toLowerCase().includes(q));
+  }
+
+  res.render("index", {
+    title: "Hasil Pencarian",
+    products: filteredProducts,
+    query: q,
+  });
 });
 
 module.exports = router;
